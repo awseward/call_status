99909a86364d50d0b13b2747dfada4e5c5411f5e
Andrew Seward <awswrd@gmail.com>
2020-05-10 22:08:04 -0700

    Shuffle some things around
    This is mostly aimless, idle shifting but I think maybe it's not the
    worst outcome, also.

--- src/backend.nim
+++ src/backend.nim
@@ -1,6 +1,5 @@
 import asyncdispatch
 import db_postgres
-import htmlgen as h
 import jester
 import json
 import os
@@ -9,83 +8,52 @@ import strutils
 
 import ./db
 let db_open = open_pg
-
-include "./views/index.html.nimf"
+import ./models/person
+import ./views/index
 
 let settings = newSettings()
 if existsEnv("PORT"):
   settings.port = Port(parseInt(getEnv("PORT")))
 
+proc setStatus(person: Person) =
+  db_open().use do (conn: DbConn):
+    conn.exec sql(
+      """
+        UPDATE people
+        SET is_on_call = $1
+        WHERE name = '$2';
+      """ % [$person.is_on_call, person.name]
+    )
+
 routes:
   get "/":
-    proc renderUser(name: string, isOnACall: bool): string =
-      let descText  =
-        if isOnACall: "is on a call"
-                else: "is not on a call"
-      let submitText =
-        if isOnACall: "Set status to \"not on a call\""
-                else: "Set status to \"on a call\""
-      let statusClass =  if isOnACall: "is-on-call"
-                                 else: ""
-
-      return h.div(
-        class = statusClass & " half",
-        h.form(
-          action   = "set_status/" & name,
-          `method` = "POST",
-          class    = statusClass,
-          h.input(
-            `type` = "hidden",
-            name="is_on_call",
-            value=($ not isOnACall)
-          ),
-          h.h1(name),
-          h.h2(descText),
-          h.details(
-            h.summary("Status not accurate?"),
-            h.button(type = "submit", submitText),
-          )
-        )
-      )
-
     let rows = db_open().use do (conn: DbConn) -> seq[Row]:
       conn.getAllRows sql"""
         SELECT
             name
           , is_on_call
         FROM people
+        WHERE name IN ('D', 'N')
         ORDER BY name
         ;
       """
-    # Meh...
-    let forms = rows.map proc (r: Row): string =
-      let name = r[0]
-      let isOn = r[1] == "t"
-      return renderUser(name, isOn)
-    resp renderIndex(forms[0], forms[1])
+    let forms = rows.map(fromPgRow).map(renderPerson)
 
-  proc setStatus(name: string, isOnCall: bool) =
-    db_open().use do (conn: DbConn):
-      conn.exec sql(
-        """
-          UPDATE people
-          SET is_on_call = $1
-          WHERE name = '$2';
-        """ % [$isOnCall, name]
-      )
+    resp renderIndex(forms[0], forms[1])
 
   post "/api/status":
     let jsonNode = parseJson request.body
+    # Maybe use logger?
     echo jsonNode
-    let user     = jsonNode["user"].getStr()
-    let isOnCall = jsonNode["is_on_call"].getBool()
-
-    setStatus user, isOnCall
+    setStatus person.fromJson(jsonNode)
     resp Http204
 
   post "/set_status/@name":
-    let isOnCall = parseBool request.params["is_on_call"]
-    setStatus @"name", isOnCall
+    let person = Person(
+      name:       @"name",
+      is_on_call: parseBool request.params["is_on_call"],
+    )
+    setStatus person
     redirect "/"
 
 runForever()
---
+++ src/views/index.nim
@@ -0,0 +1,34 @@
+import htmlgen as h
+import ../models/person
+
+include "./index.html.nimf"
+
+proc renderPerson*(person: Person): string =
+  let isOnACall = person.is_on_call
+  let descText  =
+    if isOnACall: "is on a call"
+            else: "is not on a call"
+  let submitText =
+    if isOnACall: "Set status to \"not on a call\""
+            else: "Set status to \"on a call\""
+  let statusClass =  if isOnACall: "is-on-call"
+                             else: ""
+  return h.div(
+    class = statusClass & " half",
+    h.form(
+      action   = "set_status/" & person.name,
+      `method` = "POST",
+      class    = statusClass,
+      h.input(
+        `type` = "hidden",
+        name="is_on_call",
+        value=($ not isOnACall)
+      ),
+      h.h1(person.name),
+      h.h2(descText),
+      h.details(
+        h.summary("Status not accurate?"),
+        h.button(type = "submit", submitText),
+      )
+    )
+  )
