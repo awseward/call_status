69fd47014eed8741875bed628e3dfb452f73e406
Andrew Seward <awswrd@gmail.com>
2020-05-14 23:39:31 -0700

    Further deprecations updates

--- src/deprecations.nim
+++ src/deprecations.nim
@@ -4,14 +4,18 @@ import strUtils
 
 type Deprecation = distinct string
 
-proc isSupported*(deprecation: Deprecation, default: bool = true): bool =
-  let key = "DEPRECATION_SUPPORT_" & deprecation.string
-  result = parseBool getEnv(key, default = $default)
-  if result:
+proc log(deprecation: Deprecation, supported: bool) =
+  if supported:
     warn "Deprecated (but supported) functionality triggered: ", deprecation.string
   else:
     error "Deprecated (and no longer supported) functionality triggered: ", deprecation.string
-  flushFile stdout
+
+proc isSupported*(deprecation: Deprecation, default: bool = true): bool =
+  let key = "DEPRECATION_SUPPORT_" & deprecation.string
+  result = parseBool getEnv(key, default = $default)
+  log deprecation, result
+
+# ---
 
 const USER_KEY* = Deprecation "USER_KEY"
 const API_STATUS_ENDPOINTS* = Deprecation "API_STATUS_ENDPOINTS"
