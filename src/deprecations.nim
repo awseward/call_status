f379b5cc0fa0eb329f82ffd322148fa91a672430
Andrew Seward <awswrd@gmail.com>
2020-05-14 22:56:35 -0700

    Log on unsupported case, too

--- src/deprecations.nim
+++ src/deprecations.nim
@@ -8,8 +8,10 @@ proc isSupported*(deprecation: Deprecation, default: bool = true): bool =
   let key = "DEPRECATION_SUPPORT_" & deprecation.string
   result = parseBool getEnv(key, default = $default)
   if result:
-    warn "Deprecated functionality triggered: ", deprecation.string
-    flushFile stdout
+    warn "Deprecated (but supported) functionality triggered: ", deprecation.string
+  else:
+    error "Deprecated (and no longer supported) functionality triggered: ", deprecation.string
+  flushFile stdout
 
 const USER_KEY* = Deprecation "USER_KEY"
 const API_STATUS_ENDPOINTS* = Deprecation "API_STATUS_ENDPOINTS"
