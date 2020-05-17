a5fa8969103ee8c5db6b2956f08e0f1fd1c0f637
Andrew Seward <awswrd@gmail.com>
2020-05-16 17:39:20 -0700

    Add a branch for linux
    Just to satisfy CI for the moment.
    
    To reiterate the comment:
    
    > This application doesn't actually target linux currently, but am just
    > doing this so that CI can pass since this is evaluated at compile time.
    >
    > Maybe a better thing would be to just change the CI to run on a macosx
    > container, but this is simpler and the alternative can always be
    > revisited.
    >
    > For why this differs from the macosx path, see:
    > https://www.tldp.org/HOWTO/HighQuality-Apps-HOWTO/fhs.html

--- src/checker_config.nim
+++ src/checker_config.nim
@@ -8,7 +8,24 @@ type ConfigFilepath = distinct string
 
 const DEFAULT_CONFIG_FILEPATH =
   when hostOS == "macosx":
+    # Basis of this filepath is homebrew installation directory choices.
+    #
+    # See:
+    # https://github.com/awseward/homebrew-tap/Formula/call_status_checker.rb
+    #
     ConfigFilepath "/usr/local/etc/call_status_checker/config.json"
+  elif hostOS == "linux":
+    # This application doesn't actually target linux currently, but am just
+    # doing this so that CI can pass since this is evaluated at compile time.
+    #
+    # Maybe a better thing would be to just change the CI to run on a macosx
+    # container, but this is simpler and the alternative can always be
+    # revisited.
+    #
+    # For why this differs from the macosx path, see:
+    # https://www.tldp.org/HOWTO/HighQuality-Apps-HOWTO/fhs.html
+    #
+    ConfigFilepath "/etc/call_status_checker/config.json"
   else:
     raise Exception.newException "Unsupported host OS: " & hostOS
 
