bce09f5d356f62a360ded2f7aa7706949e740103
Andrew Seward <awswrd@gmail.com>
2020-05-25 22:27:03 -0700

    Use junk_drawer/source_info

--- src/statics.nim
+++ src/statics.nim
@@ -1,24 +1,5 @@
-import parsecfg
-import streams
-import strutils
-import os
+import junk_drawer/source_info
 
-proc getPkgVersion(): string {.compileTime.} =
-  const filepath = staticExec("ls ../*.nimble").splitLines()[0]
-  var stream: StringStream
-  try:
-    stream = newStringStream slurp(filePath)
-    let cfg = loadConfig stream
-    cfg.getSectionValue("", "version")
-  finally:
-    close stream
+const pkgVersion* = getNimbleVersion()
 
-proc getPkgRevision(): string {.compileTime.} =
-  let fromEnv = getEnv "SOURCE_VERSION"
-  if fromEnv != "": fromEnv
-  else:
-    staticExec "git rev-parse HEAD"
-
-const pkgVersion* = getPkgVersion()
-
-const pkgRevision* = getPkgRevision()
+const pkgRevision* = getGitRevision()
