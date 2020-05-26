dfec4371313f615af4cbf9488342eb5836045948
Andrew Seward <awswrd@gmail.com>
2020-05-25 23:13:43 -0700

    Revert "Use junk_drawer/source_info"
    This reverts commit bce09f5d356f62a360ded2f7aa7706949e740103.

--- src/statics.nim
+++ src/statics.nim
@@ -1,5 +1,24 @@
-import junk_drawer/source_info
+import parsecfg
+import streams
+import strutils
+import os
 
-const pkgVersion* = getNimbleVersion()
+proc getPkgVersion(): string {.compileTime.} =
+  const filepath = staticExec("ls ../*.nimble").splitLines()[0]
+  var stream: StringStream
+  try:
+    stream = newStringStream slurp(filePath)
+    let cfg = loadConfig stream
+    cfg.getSectionValue("", "version")
+  finally:
+    close stream
 
-const pkgRevision* = getGitRevision()
+proc getPkgRevision(): string {.compileTime.} =
+  let fromEnv = getEnv "SOURCE_VERSION"
+  if fromEnv != "": fromEnv
+  else:
+    staticExec "git rev-parse HEAD"
+
+const pkgVersion* = getPkgVersion()
+
+const pkgRevision* = getPkgRevision()
