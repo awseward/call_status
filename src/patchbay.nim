77919397404b9a5be3af05a76a3fba60cc6596c5
Andrew Seward <awswrd@gmail.com>
2020-06-16 00:07:50 -0700

    Revert "Maybe nonblocking won't work out"
    This reverts commit ea0affb898d349da9b625e41af6c1129b94ec9f4.

--- src/patchbay.nim
+++ src/patchbay.nim
@@ -46,4 +46,4 @@ proc foo*(path: string, json: JsonNode) =
   for channelId in getChannelIds():
     let uri = pubsubBaseUri / channelId.string / path
     debug httpMethod, " ", uri
-    discard waitFor http.request($uri, httpMethod, body = $json)
+    asyncCheck http.request($uri, httpMethod, body = $json)
