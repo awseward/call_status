b0217b48b5d6b35b95db32c02ed25a6c5cd20a7a
Andrew Seward <awswrd@gmail.com>
2020-06-24 00:40:52 -0700

    random: Freshen up heroku_bootstrap.sh

--- scripts/heroku_bootstrap.sh
+++ scripts/heroku_bootstrap.sh
@@ -9,10 +9,11 @@ git remote show heroku \
 heroku create --buildpack heroku/nodejs
 heroku buildpacks:add https://github.com/awseward/heroku-buildpack-nim
 heroku addons:create heroku-postgresql:hobby-dev
+heroku addons:create heroku-redis:hobby-dev
 
 heroku config:set IS_HEROKU=true
 heroku config:set NIM_BRANCH=devel
 heroku config:set NIM_REV=dc3919bb1af89799e391b4c4ecd0f1f60f7862ff
-heroku config:set NIMBLE_FLAGS=-d:release
+heroku config:set NIMBLE_FLAGS='-d:release -d:ssl'
 
 git push heroku master
