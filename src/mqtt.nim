25b75a927f3d28eaf72e98511040be2eb2ddb36a
Andrew Seward <awswrd@gmail.com>
2020-06-26 01:07:16 -0700

    wip: Welp

--- src/mqtt.nim
+++ src/mqtt.nim
@@ -6,6 +6,8 @@ import os
 import strutils
 import uri
 
+import ./logs
+
 type MqttInfo* = object
   host*: string
   port*: int
@@ -19,11 +21,17 @@ let configured* = block:
     topic: getEnv "MQTT_TOPIC"
   )
 
-let clientId = "clientId-callstatus-" & $genOid()
+let clientId = "uhhh" & $genOid()
 
-proc mqttPublish*(json: JsonNode) {.async.} =
+proc mqttPublish*(jNode: JsonNode) {.async.} =
   let ctx = newMqttCtx clientId
   ctx.set_host(configured.host, configured.port)
   await ctx.start()
-  await ctx.publish(configured.topic, $json, qos = 1, retain = true)
+  debug "configured: ", configured
+  debug "isConnected: ", ctx.isConnected
+  # debug "Publishing to ", configured.host, ":", configured.port, " [", configured.topic, "]: ", $json
+  await ctx.publish(configured.topic, $jNode, qos = 2, retain = true)
+  await sleepAsync 500
   await ctx.disconnect()
+
+# waitFor mqttPublish(%*{"foo": "wut"})
