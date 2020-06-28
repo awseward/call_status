3ee54a85ccbefe466fdd595aeafc86b919481c1e
Andrew Seward <awswrd@gmail.com>
2020-06-28 07:30:23 -0700

    Fix poll script
    Turns out xargs was mangling the json, probably by not preserving quotes
    where desired.

--- rpi/poll_call_status.sh
+++ rpi/poll_call_status.sh
@@ -2,8 +2,13 @@
 
 set -euo pipefail
 
-# Run as systemd service (see: https://www.linode.com/docs/quick-answers/linux/start-service-at-boot/)
-# See also: https://gist.github.com/awseward/100cec20c2523b0c5c5bc083dd114ae8/20da7082070affdb259013b00abbdba7d4352ddc
+# Run as systemd service
+#
+# See also:
+#   - https://www.linode.com/docs/quick-answers/linux/start-service-at-boot/
+#   - https://gist.github.com/awseward/100cec20c2523b0c5c5bc083dd114ae8/20da7082070affdb259013b00abbdba7d4352ddc
+#   - https://web.archive.org/web/20200628142802/https://ma.ttias.be/auto-restart-crashed-service-systemd/
+#
 
 api_url='https://call-status.herokuapp.com/api/people'
 mqtt_topic='call-status/people'
@@ -13,7 +18,7 @@ echo "Started at $(date --iso-8601=s)" | systemd-cat -t poll_call_status -p info
 while true; do
   echo "${api_url}" \
     | xargs -t curl -s \
-    | xargs -t mosquitto_pub -h localhost -t "${mqtt_topic}" -r -m
+    | mosquitto_pub -h localhost -t "${mqtt_topic}" -r -s
 
   sleep 5
 done
