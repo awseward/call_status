784fa4a4ca323f8a71bd3e64325608a68d1bba96
Andrew Seward <awswrd@gmail.com>
2020-12-16 20:36:18 -0800

    Pull dw-misc files as part of build/deploy

--- .heroku/run.sh
+++ .heroku/run.sh
@@ -17,3 +17,14 @@ mv uplink .bin/
 rm -f uplink_linux_amd64.zip
 
 which uplink && uplink version
+
+### Install files from dw-misc
+
+readonly dw_misc_ver='6b0b3f107f8c3bebe1f482621e691368061b87a1'
+
+curl "https://github.com/awseward/dw-misc/raw/${dw_misc_ver}/bin/dw_push_sqlite" \
+  -L -o .bin/dw_push_sqlite \
+  && chmod 511 .bin/dw_push_sqlite
+curl "https://github.com/awseward/dw-misc/raw/${dw_misc_ver}/bin/dw_signal_sqlite" \
+  -L -o .bin/dw_signal_sqlite \
+  && chmod 511 .bin/dw_signal_sqlite
