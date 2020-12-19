293416a7431829818a80a13259da9d7bf1ed412e
Andrew Seward <awswrd@gmail.com>
2020-12-19 00:22:50 -0800

    Use dw_push

--- .heroku/run.sh
+++ .heroku/run.sh
@@ -12,19 +12,26 @@ echo "PATH=${PATH}"
 
 curl -L https://github.com/storj/storj/releases/latest/download/uplink_linux_amd64.zip -o uplink_linux_amd64.zip
 unzip -o uplink_linux_amd64.zip
-chmod +x uplink
 mv uplink .bin/
-rm -f uplink_linux_amd64.zip
+rm -vf uplink_linux_amd64.zip
 
 which uplink && uplink version
 
 ### Install files from dw-misc
 
-readonly dw_misc_ver='6b0b3f107f8c3bebe1f482621e691368061b87a1'
+dw_bin_install() {
+  local -r dw_misc_ver='644bd6fb3209a0c1fc8ac274239eb7fc40b12584'
+  local -r relpath="$1"
+  local -r url="https://github.com/awseward/dw-misc/raw/${dw_misc_ver}/bin/${relpath}"
+  local -r outpath=".bin/${relpath}"
 
-curl "https://github.com/awseward/dw-misc/raw/${dw_misc_ver}/bin/dw_push_sqlite" \
-  -L -o .bin/dw_push_sqlite \
-  && chmod 511 .bin/dw_push_sqlite
-curl "https://github.com/awseward/dw-misc/raw/${dw_misc_ver}/bin/dw_signal_sqlite" \
-  -L -o .bin/dw_signal_sqlite \
-  && chmod 511 .bin/dw_signal_sqlite
+  mkdir -p "$(dirname "${outpath}")"
+
+  curl "${url}" -L -o "${outpath}"
+}
+
+dw_bin_install 'dw_push_sqlite'
+dw_bin_install 'dw_signal_sqlite'
+dw_bin_install 'dw_push'
+
+chmod -v 500 .bin/*
