55f130e1d0b90832bad18c9735ce215bb81ca53d
Andrew Seward <awswrd@gmail.com>
2021-05-02 18:54:07 -0700

    Run nimble pretty

--- src/xdg_dirs.nim
+++ src/xdg_dirs.nim
@@ -2,4 +2,4 @@ import os
 
 let CacheHome* = os.getEnv("XDG_CACHE_HOME", default = os.expandTilde "~/.cache/")
 let ConfigHome* = os.getConfigDir()
-let DataHome* = os.getEnv("XDG_DATA_HOME",  default = os.expandTilde "~/.local/share")
+let DataHome* = os.getEnv("XDG_DATA_HOME", default = os.expandTilde "~/.local/share")
