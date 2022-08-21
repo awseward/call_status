6b0c06ac1693fee3ce672470f8f7d936e599156b
Andrew Seward <awswrd@gmail.com>
2022-08-21 13:51:25 -0700

    Touch up scripts a bit

--- rpi/call_status.sh
+++ rpi/call_status.sh
@@ -16,37 +16,54 @@ readonly topic_heartbeat_latest="${topic_heartbeat}/latest"
 readonly topic_people='call-status/people'
 
 _info() { systemd-cat -t "call_status<${1}>" -p info ; }
+# This can be used in place of the above for dev on a non-systemd env
+# _info() { >&2 echo -n 'info: '; >&2 cat; }
 _start_msg() { date --iso-8601=s | xargs echo 'Started at' ; }
+
+_pub() {
+  mosquitto_pub \
+    --host localhost \
+    --retain \
+    "$@"
+}
+
+_sub() {
+  mosquitto_sub \
+    --host localhost \
+    "$@"
+}
+
+_to_elapsed_s() {
+  jq -r '.timestamp' \
+  | xargs -I{} date -d {} +%s \
+  | jq --argjson now_s "$(date +%s)" '$now_s - .'
+}
+
+fetch_if_enabled() {
+  _name="${FUNCNAME[0]}"; info() { _info "$_name" ; }
+  local -r enabled="$1"
+
+  info <<< "enabled: $enabled"
+
+  if [ "$enabled" = 'false' ]; then
+    info <<< 'doing nothing…'
+  else
+    info <<< "${api_url_people} >> ${topic_people}"
+    echo "${api_url_people}" \
+    | xargs -t curl -fsS -XGET \
+    | _pub --stdin-file --topic "${topic_people}"
+  fi
+}
 
 poll_statuses() {
   _name="${FUNCNAME[0]}"; info() { _info "$_name" ; }
   _start_msg | info
 
   while true; do
-    now_s="$(date +%s)"
-    latest="$(mosquitto_sub -t "${topic_heartbeat_latest}" -C 1 -W 1 | jq -r .timestamp)"
-
-    info <<< "latest heartbeat: ${latest}"
-
-    if [ "${latest}" == '' ]; then
-      info <<< 'no latest heartbeat; doing nothing…'
-    else
-      latest_s="$(date -d "${latest}" +%s)"
-      diff_s="$(( now_s - latest_s ))"
-
-      info <<< "now_s:    ${now_s}"
-      info <<< "latest_s: ${latest_s}"
-      info <<< "diff_s:   ${diff_s}"
-
-      if [ $diff_s -gt 10 ]; then
-        info <<< 'more than 10s since latest heartbeat; doing nothing…'
-      else
-        info <<< "polling: ${api_url_people} >> ${topic_people}"
-        echo "${api_url_people}" \
-          | xargs -t curl -s \
-          | mosquitto_pub -h localhost -t "${topic_people}" -r -s
-      fi
-    fi
+    _sub --topic "${topic_heartbeat_latest}" -C 1 -W 1 \
+    | _to_elapsed_s \
+    | jq '. <= 10' \
+    | xargs -t "$0" fetch_if_enabled
 
     sleep 5
   done
@@ -56,13 +73,14 @@ poll_heartbeats() {
   _name="${FUNCNAME[0]}"; info() { _info "$_name" ; }
   _start_msg | info
 
-  while true; do
-    mosquitto_sub -t "${topic_heartbeat}" | while read -r line; do
-      msg="$(echo "${line}" | jq -c --arg timestamp "$(date --iso-8601=s)" '. + { $timestamp }')"
-      mosquitto_pub -t "${topic_heartbeat_latest}" -r -m "${msg}"
-    done
-    info <<< "subscription dropped for some reason; resubscribing…"
-  done
+  _sub --topic "${topic_heartbeat}" \
+  | while read -r msg; do
+      jq \
+        --compact-output \
+        --arg timestamp "$(date --iso-8601=s)" \
+        '{ $timestamp } + .' <<< "${msg}"
+    done \
+  | _pub --stdin-line --topic "${topic_heartbeat_latest}"
 }
 
 "$@"
