8b51de8e3df4e57ca88d3678f521b9c04b6e4fd8
Andrew Seward <awswrd@gmail.com>
2022-08-21 16:34:32 -0700

    Make some more tweaks

--- rpi/call_status.sh
+++ rpi/call_status.sh
@@ -10,18 +10,29 @@ set -euo pipefail
 #   - https://web.archive.org/web/20200628142802/https://ma.ttias.be/auto-restart-crashed-service-systemd/
 #
 
-readonly api_url_people='https://call-status.herokuapp.com/api/people'
-readonly topic_heartbeat='call-status/heartbeat'
-readonly topic_heartbeat_latest="${topic_heartbeat}/latest"
-readonly topic_people='call-status/people'
+export API_URL_PEOPLE="${API_URL_PEOPLE:-https://call-status.herokuapp.com/api/people}"
+export TOPIC_HEARTBEAT="${TOPIC_HEARTBEAT:-call-status/heartbeat}"
+export TOPIC_HEARTBEAT_LATEST="${TOPIC_HEARTBEAT_LATEST:-${topic_heartbeat}/latest}"
+export TOPIC_PEOPLE="${TOPIC_PEOPLE:-call-status/people}"
 
 set +e
 if (type -f systemd-cat >/dev/null 2>&1); then
-  _info() { systemd-cat -t "call_status<$1>" -p info ; }
+  _log() {
+    local -r level="$1"
+    local -r label="$2"
+    systemd-cat -t "call_status<$label>" -p "$level" ;
+  }
 else
-  _info() { >&2 echo -n "$0<$1>: "; >&2 cat; }
+  _log() {
+    local -r level="$1"
+    local -r label="$2"
+    >&2 echo -n "[${level^^}] $0<$label>: "; >&2 cat;
+  }
 fi
 set -e
+
+_info()  { _log info "$@";  }
+_debug() { _log debug "$@"; }
 
 # This can be used in place of the above for dev on a non-systemd env
 _start_msg() { date --iso-8601=s | xargs echo 'Started at' ; }
@@ -54,15 +65,13 @@ fetch_if_enabled() {
   _name="${FUNCNAME[0]}"; info() { _info "$_name" ; }
   local -r enabled="$1"
 
-  info <<< "enabled: $enabled"
-
   if [ "$enabled" = 'false' ]; then
     info <<< 'doing nothing…'
   else
-    info <<< "${api_url_people} >> ${topic_people}"
-    echo "${api_url_people}" \
+    info <<< "${API_URL_PEOPLE} >> ${TOPIC_PEOPLE}"
+    echo "${API_URL_PEOPLE}" \
     | xargs -t curl -fsS -XGET \
-    | _pub --stdin-file --topic "${topic_people}"
+    | _pub --stdin-file --topic "${TOPIC_PEOPLE}"
   fi
 }
 
@@ -70,28 +79,27 @@ poll_statuses() {
   _name="${FUNCNAME[0]}"; info() { _info "$_name" ; }
   _start_msg | info
 
-  _sub --topic "${topic_heartbeat_latest}" \
+  _sub --topic "${TOPIC_HEARTBEAT_LATEST}" \
   | _to_elapsed_s \
   | jq --unbuffered '. <= 10' \
   | while read -r enabled; do
       xargs -t "$0" fetch_if_enabled <<< "$enabled"
-      sleep 5
     done
-
 }
 
 poll_heartbeats() {
   _name="${FUNCNAME[0]}"; info() { _info "$_name" ; }
   _start_msg | info
 
-  _sub --topic "${topic_heartbeat}" \
+  _sub --topic "${TOPIC_HEARTBEAT}" \
   | while read -r msg; do
       jq \
         --compact-output \
         --arg timestamp "$(date --iso-8601=s)" \
-        '{ $timestamp } + .' <<< "${msg}"
+        '{ $timestamp } + .' <<< "${msg}" \
+      | tee >("$0" _debug "$_name")
     done \
-  | _pub --stdin-line --topic "${topic_heartbeat_latest}"
+  | _pub --stdin-line --topic "${TOPIC_HEARTBEAT_LATEST}"
 }
 
 "$@"
